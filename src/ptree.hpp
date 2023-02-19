2a52a47a9fd66043bb675132a21fa39a49fd33d0
Evgeniy Kobi <bogdanov.es@phystech.edu>
2023-02-19 19:44:14 +0300

    Base class PTree fix. Now it compiles

--- src/ptree.hpp
+++ src/ptree.hpp
@@ -1,15 +1,29 @@
 #pragma once
 
 #include <string>
+#include <cstring>
 
 namespace ptree {
 class PTree {
   public:
   PTree* parent;
   PTree *left, *right;
-  virtual ~PTree();
-  virtual PTree* execute();
-  virtual bool isLeaf() const;
-  virtual std::string dump() const;
+  virtual ~PTree() = default;
+  virtual PTree* execute() {}
+  virtual bool isLeaf() const {
+    return 0;
+  }
+  virtual std::string dump() const {
+    return std::string{""};
+  }
+  int getid() const {
+    int id;
+    memcpy(&id, this, sizeof(int));
+    return id;
+  }
+  std::string getname() const {
+    int id = getid();
+    return "node" +  std::to_string(id);
+  }
 };
 };
