ffd155d8bea6a134d85482df1db6419a56b7c337
Evgeniy Kobi <bogdanov.es@phystech.edu>
2023-04-18 22:12:12 +0300

    Split leaf.hpp to .hpp and .cpp

--- modules/paracl/leaf.hpp
+++ modules/paracl/leaf.hpp
@@ -10,15 +10,12 @@
 
 
 namespace ptree {
+
 class Leaf : public PTree {
 public:
-  Leaf(PTree* parent = nullptr): PTree(parent, nullptr, nullptr) {};
-  virtual bool isLeaf() const override {
-    return true;
-  }
-  virtual std::unique_ptr<PTree> execute(Stack *stack = nullptr) const override {
-    return std::unique_ptr<PTree>{};
-  }
+  Leaf(PTree* parent = nullptr);
+  virtual bool isLeaf() const override;
+  virtual std::unique_ptr<PTree> execute(Stack *stack = nullptr) const override;
 };
 
 template <typename T> class Imidiate : public Leaf {
@@ -43,14 +40,7 @@ public:
   }
 };
 
-std::unique_ptr<PTree> intinput() {
-  int x;
-  std::cin >> x;
-#ifdef DBG_CALL
-  std::cout << "Input called" << std::endl;
-#endif
-  return std::unique_ptr<PTree>(new Imidiate<int>(x)); 
-}
+std::unique_ptr<PTree> intinput();
 
 class Reserved : public Leaf {
 public:
@@ -61,40 +51,11 @@ public:
 private:
   Types type_;
 public:
-  Reserved(PTree* parent = nullptr, Types type = Types::None) : Leaf(parent), type_(type) {}
-  Types gettype() const {
-    return type_;
-  }
-  std::string typetostr() const {
-    switch(gettype()) {
-      case Types::None:
-        return "None";
-        break;
-      case Types::Input:
-        return "Input";
-        break;
-    }
-    return "Smth strange";
-  }
-  virtual std::unique_ptr<PTree> execute(Stack *stack = nullptr) const override {
-#ifdef DBG_CALL
-    std::cout << "Resrved called" << std::endl;
-#endif
-    switch(gettype()) {
-      case Types::None:
-        return std::unique_ptr<PTree>{};
-      case Types::Input:
-        return intinput();
-    }
-    return std::unique_ptr<PTree>{};
-  }
-  virtual std::string dump() const override {
-    std::string res;
-    std::string parentname = getparent()->getname();
-    std::string myname = getname();
-    res += myname + " [label=" + '"' + typetostr() + '"' + "];\n";
-    return res;
-  }
+  Reserved(PTree* parent = nullptr, Types type = Types::None);
+  Types gettype() const;
+  std::string typetostr() const;
+  virtual std::unique_ptr<PTree> execute(Stack *stack = nullptr) const override;
+  virtual std::string dump() const override;
 };
 
 class NameInfo : public Leaf {
@@ -103,54 +64,29 @@ private:
   int offset_;
   std::string name_;
 protected:
-  NameInfo(PTree* parent = nullptr, std::string name = "") : Leaf(parent), name_(name) {};
-  NameInfo(PTree* parent, int nameid, int offset, std::string name = "") : Leaf(parent), nameid_(nameid), offset_(offset), name_(name) {};
+  NameInfo(PTree* parent = nullptr, std::string name = "");
+  NameInfo(PTree* parent, int nameid, int offset, std::string name = "");
 public:
-  int getnameid() const {
-    return nameid_;
-  }
-  int getoffset() const {
-    return offset_;
-  }
-  void setoffset(int offset) {
-    offset_ = offset;
-  }
-  std::string getvarname() const {
-    return name_;
-  }
+  int getnameid() const;
+  int getoffset() const;
+  void setoffset(int offset);
+  std::string getvarname() const;
 };
 
 class NameInt : public NameInfo {
 private:
   int value_;
 public:
-  NameInt(PTree* parent = nullptr, int value = 0, std::string name_ = "") : NameInfo(parent, name_), value_(value) {};
-  NameInt(PTree* parent, int value, int nameid, int offset, std::string name_ = "") : NameInfo(parent, nameid, offset, name_), value_(value) {};
-  int getvalue() const { //returns value from the field that value equals to value in stack
-    return value_;
-  }
-  int getvalue(Stack *stack) const { //returns value from the stack
-    int value;
-    stack->read(getoffset(), value);
-    return value;
-  }
-  void setvalue(int value, Stack *stack) {
-    stack->write(getoffset(), value);
-    value_ = value;
-  }
-  virtual std::string dump() const override {
-    std::string res;
-    std::string parentname = getparent()->getname();
-    std::string myname = getname();
-    res += myname + " [label=" + '"' + getvarname() + '=' + std::to_string(getvalue()) + '\n' + "offset: " + std::to_string(getoffset()) +'"' + "];\n";
-    return res;
-  }
+  NameInt(PTree* parent = nullptr, int value = 0, std::string name_ = "");
+  NameInt(PTree* parent, int value, int nameid, int offset, std::string name_ = "");
+  //returns value from the field that value equals to value in stack
+  int getvalue() const;
+  //returns value from the stack
+  int getvalue(Stack *stack) const;
+  void setvalue(int value, Stack *stack);
 
-  std::unique_ptr<PTree> execute(Stack *stack) const override {
-    return std::unique_ptr<PTree>{new Imidiate<int>(getvalue(stack))};
-  }
+  virtual std::string dump() const override;
+  std::unique_ptr<PTree> execute(Stack *stack) const override;
 };
-
-
 
 }
