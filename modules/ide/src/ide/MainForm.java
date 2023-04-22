de061565b8c1645fd37ebffcf6781896cde5707b
Evgeniy Kobi <bogdanov.es@phystech.edu>
2023-04-22 22:26:13 +0300

    Delete setColor function because of critical bug

--- modules/ide/src/ide/MainForm.java
+++ modules/ide/src/ide/MainForm.java
@@ -312,6 +312,7 @@ public class MainForm extends javax.swing.JFrame
   
   private void setColor(Color color)
   {
+    return;
     Color brightColor = color.brighter();
     Color darkColor = color.darker();
     textAreaInput.setBackground(color);
